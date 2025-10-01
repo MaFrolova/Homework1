de831c572cd073b92c50a4afc9b71e16c6017000
MaFrolova <mariyafrolova2308@yandex.ru>
2025-10-01 20:22:24 +0300

    создана 6я игра

---
+++ games.js
@@ -0,0 +1,215 @@
+//Game1
+function game1() {
+    
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+        while (true) {
+            let guessNumber = prompt("Угадай число от 1 до 100");
+
+            guessNumber = Number(guessNumber);
+
+            if (guessNumber === randomNumber) {
+
+                alert("Поздравляю, ты угадал число!");
+
+                break;
+
+            } else if (guessNumber > randomNumber) {
+
+                alert("Твое число больше загаданного, попробуй еще!");
+
+            } else {
+
+                alert("Твое число меньше загаданного, попробуй еще!");
+            }
+        }
+}
+
+console.log(randomNumber);
+
+//Game2
+function ariphmetic() {
+    const operations = ["+", "-", "*", "/"];
+
+    const operation = operations[Math.floor(Math.random() * operations.length)];
+    
+    const a = Math.floor(Math.random() * 10) + 1;
+    const b = Math.floor(Math.random() * 10) + 1;
+    
+    let result;
+    let userAnswer;
+
+    switch (operation) {
+        case '+':
+            userAnswer = prompt (`Сложите ${a} + ${b}`);
+            result = a + b;
+            break;
+
+        case '-':
+            if (a < b) {
+                [a, b] = [b, a];
+            }
+            userAnswer = prompt (`Вычтите ${a} - ${b}`);
+            result = a - b;
+            break;
+
+        case '*':
+            userAnswer = prompt (`${a} умножить на ${b}`);
+            result = a * b;
+            break;
+
+        case '/':
+            if (a < b) {
+                [a, b] = [b, a]; 
+            }
+            if (a % b !== 0) {
+                return arithmetic();
+            }
+            userAnswer = prompt (`${a} разделить на ${b}`);
+            result = a / b;
+            break;
+    }
+
+    const userNumber = parseFloat(userAnswer);
+
+    if (userNumber === result) {
+            alert('Это верный ответ!');
+        } else {
+            alert(`Ошибка! Правильный ответ: ${result}`);
+        }
+    }
+
+//Game3 
+function turnText() {
+    let userInput;
+    do {
+        userInput = prompt("Введите ваше слово или фразу:");
+        
+        if (userInput === '') {
+            alert("Вы ничего не ввели! Пожалуйста, введите текст.");
+        } 
+        
+        else if (!isNaN(userInput)) {
+            alert("Вы ввели число. Пожалуйста, введите слово или фразу.");
+        } 
+        else {
+            const reversedText = userInput
+                .split('')  
+                .reverse()   
+                .join('');   
+            
+            alert(`Исходный текст: "${userInput}"\nПеревернутый текст: "${reversedText}"`);
+            break;
+        }
+    } while (true);
+}
+
+//Game4
+function stone() {
+    const options = ['камень', 'ножницы', 'бумага'];       
+    const compIndex = Math.floor(Math.random() * options.length);
+    const compAnswer = options[compIndex];
+    
+    let userChoice = prompt('Введите вариант: Камень, ножницы, бумага!').toLowerCase();
+    
+    const results = (user, computer) => {
+        if (!options.includes(user)) {
+            return 'Вы ввели что-то не то!';
+        }
+        
+        if (user === computer) {
+            return `Компьютер выбрал "${compAnswer}". Вы выбрали "${userChoice}".
+            Ничья!`;
+        }
+        
+        const winAnswer = {
+            камень: 'ножницы',
+            ножницы: 'бумага',
+            бумага: 'камень'
+        };
+        
+        if (winAnswer[user] === computer) {
+            return `Компьютер выбрал "${compAnswer}". Вы выбрали "${userChoice}".
+        Вы выиграли!`;
+        }
+        
+        return `Компьютер выбрал "${compAnswer}". Вы выбрали "${userChoice}".
+        Вы проиграли! `;
+    };
+
+    const result = results(userChoice, compAnswer);
+    console.log(result);
+    alert(result);
+}
+
+//Game5
+function quizGame() {
+    
+    let userAnswer;
+    let startMessage;
+    let pointCounter = 0;
+    let sum;
+
+    const quiz = [
+        {
+            question: "Какой цвет небо?",
+            options: ["1. Красный", " 2. Синий", " 3. Зеленый"],
+            correctAnswer: 2 
+        },
+        {
+            question: "Сколько дней в неделе?",
+            options: ["1. Шесть", " 2. Семь", " 3. Восемь"],
+            correctAnswer: 2
+        },
+        {
+            question: "Сколько у человека пальцев на одной руке?",
+            options: ["1. Четыре", " 2. Пять", " 3. Шесть"],
+            correctAnswer: 2
+        }
+    ];
+
+   do {
+    startMessage = confirm("Хотите поиграть в игру Викторина?");
+    if (startMessage === true) {
+            for (let i = 0; i < quiz.length; i++) {
+                userAnswer = Number(prompt(`Ответьте на вопрос: ${quiz[i].question}, \nВарианты ответа: \n${quiz[i].options} \n(введите номер ответа)`));
+             if (userAnswer === quiz[i].correctAnswer) {
+                alert("Правильно!");
+                pointCounter++;
+                sum = pointCounter;
+                alert(`Вы получили ${pointCounter} балл(а)`);
+                
+             } else {
+                alert("Неправильный ответ!");
+             }
+            }
+            alert(`Вы набрали всего: ${sum} балл(а). Поздравляем!`);
+    } else {
+        alert("Всего хорошего!");
+        break;
+    }
+   } while (true);    
+}
+   
+//Game6
+let clicksCounter = 0;
+function getRandomRgb() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+}
+function changeBlockColor() {
+    const block = document.querySelector(".mini-game");
+    block.style.backgroundColor = getRandomRgb();
+    clicksCounter ++;
+    if(clicksCounter >= 10) {
+        endGame();
+    }
+}   
+function endGame() {
+    const block = document.querySelector(".mini-game");
+    block.style.backgroundColor = "";
+        alert("Игра завершена!");
+    clicksCounter = 0;
+}
+
